8bdcecb79cb3522fab21d5f233796e15d726919d
TatisLesmes <tatis26lesmesmail.com>
2024-07-05 22:30:23 -0500

    Cambio secretary

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,21 +5,21 @@ const fs = require("fs");
 
 
 router.get("/", (req, res) => {
-    res.render("index.ejs");
-    
-  });
+  res.render("index.ejs");
 
-  router.get("/noticias", (req, res) => {
-    res.render("noticias.ejs");
-  });
+});
 
-  router.get("/ver", (req, res) => {
-    res.render("Admin.ejs");
-  }); 
-  // Redirecciona a la pagina de la secretaria.ejs
-  router.get("/verSecretaria", (req, res) => {
-    res.render("secretaria.ejs");
-  });
+router.get("/noticias", (req, res) => {
+  res.render("noticias.ejs");
+});
+
+router.get("/ver", (req, res) => {
+  res.render("Admin.ejs");
+});
+// Redirecciona a la pagina de la secretaria.ejs
+router.get("/secretaria", (req, res) => {
+  res.render("secretary.ejs");
+});
 
 
-  module.exports = router;
+module.exports = router;
